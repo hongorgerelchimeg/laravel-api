112ca465eebb046431b8e6dd9cb262b7bf611e98
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-05-30 18:52:45 +0200

    first commit

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.admin')
+
+@section('pageTitle', 'Index')
+
+@section('pageContent')
+    <div class="container">
+        @if (session('deleted'))
+            <div class="alert alert-warning">{{ session('deleted') }}</div>
+        @endif
+
+        <form action="" method="get">
+            <div class="row row-cols-3 my-3 g-3">
+                <div class="col">
+                    <select class="form-select" aria-label="Default select example" name="author" id="author">
+                        <option value="" selected>Select an author</option>
+                        @foreach ($users as $user)
+                            <option value="{{ $user->id }}" @if($user->id == $request->author) selected @endif>{{ $user->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="col">
+                    <input type="text" class="form-control" id="search-string" name="search" placeholder="{{ __('Search') }}" value="{{ $request->search }}">
+                </div>
+
+                <div class="col">
+                    <button class="btn btn-primary">Search</button>
+                </div>
+            </div>
+            <div class="checkbox-container my-3">
+
+                @foreach ($tags as $tag)
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="checkbox[]" id="{{$tag->slug}}" value="{{$tag->id}}"
+                        @if ($request->checkbox !== Null && in_array($tag->id, old('checkbox', $request->checkbox))) checked @endif
+                        >
+                        <label class="form-check-label" for="{{$tag->slug}}">{{$tag->name}}</label>
+                    </div>
+                @endforeach
+            </div>
+        </form>
+
+        <div class="row">
+            <div class="col">
+                <table class="table table-dark table-hover">
+                    <thead>
+                        <tr>
+                        <th class="text-center" scope="col">#</th>
+                        <th class="text-center" scope="col">Title</th>
+                        <th class="text-center" scope="col">Slug</th>
+                        <th class="text-center" scope="col">Created At</th>
+                        <th class="text-center" scope="col">Updated At</th>
+                        <th class="text-center" scope="col" colspan="3">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr data-id="{{ $post->slug }}">
+                                <th class="text-center" scope="row">{{ $post->id }}</th>
+                                <td>{{ $post->title }}</td>
+                                <td>{{ $post->slug }}</td>
+                                <td>{{ date('d/m/Y', strtotime($post->created_at)) }}</td>
+                                <td>{{ date('d/m/Y', strtotime($post->updated_at)) }}</td>
+                                <td>
+                                    <a class="btn btn-primary" href="{{ route('admin.posts.show', $post->slug) }}">View</a>
+                                </td>
+                                <td>
+                                    <a class="btn btn-primary" href="{{ route('admin.posts.edit', $post->slug) }}">Edit</a>
+                                </td>
+                                <td class="text-center">
+                                    <button class="btn btn-danger btn-delete">Delete</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        {{ $posts->links() }}
+
+        <section id="confirmation-overlay" class="overlay d-none">
+            <div class="popup">
+                <h1>Sei sicuro di voler eliminare?</h1>
+                <div class="d-flex justify-content-center">
+                    <button id="btn-no" class="btn btn-primary me-3">NO</button>
+                    <form method="POST" data-base="{{ route('admin.posts.destroy', '*****') }}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger">SI</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </div>
+@endsection
